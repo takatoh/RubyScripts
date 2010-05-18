7dff738d452882eebadcc010990581a8c6cf52f3
takatoh <takatoh@mbg.nifty.com>
2010-05-18 18:16:19 +0900

    bin/rwhich.rb: New.

---
+++ bin/rwhich.rb
@@ -0,0 +1,38 @@
+#!ruby
+
+require 'optparse'
+
+
+def surfix(name)
+  /\.rb\z/i.match(name) ? name : name + ".rb"
+end
+
+
+target = ENV['PATH'].split(";")
+
+psr = OptionParser.new
+psr.on('-l', '--library', 'search library.'){ target = $LOAD_PATH }
+psr.parse!
+
+if ARGV.empty?
+  print psr
+  exit
+else
+  filename = surfix(ARGV.shift)
+end
+
+script = nil
+target.each do |dir|
+  s = "#{dir}/#{filename}"
+  if File.exist?(s)
+    script = s
+    break
+  end
+end
+
+if script
+  puts script.gsub("\\", "/")
+else
+  puts "Scritp not found  -  #{filename}"
+end
+
