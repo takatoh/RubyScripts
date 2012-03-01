634c9aa3b8b3dd5f495bc8dababa252f1430d048
takatoh <takatoh.m@gmail.com>
2012-03-01 18:08:28 +0900

    bin/httphead.rb: --status option.

--- bin/httphead.rb
+++ bin/httphead.rb
@@ -9,14 +9,30 @@
 # <tt>gem install httpclient</tt>
 #
 
-#require 'rubygems'
+SCRIPT_VERSION = "v0.1.1"
+
 require 'httpclient'
+require 'optparse'
+
+
+options = {}
+opts = OptionParser.new
+opts.banner =<<EOB
+Usage: #{opts.program_name} [options] <url>
+EOB
+opts.on('-s', '--status', 'Show status instead headers.'){ options[:status] = true }
+opts.on_tail('-h', '--help', 'Show thismessage.'){ print opts; exit }
+opts.on_tail('-v', '--version', 'Show version.'){ puts SCRIPT_VERSION; exit }
+opts.parse!
 
 
 url = ARGV.shift
 
 clnt = HTTPClient.new
-clnt.head(url).header.all.each do |name, value|
-  puts "#{name}: #{value}"
+if options[:status]
+  puts clnt.head(url).status
+else
+  clnt.head(url).header.all.each do |name, value|
+    puts "#{name}: #{value}"
+  end
 end
-
