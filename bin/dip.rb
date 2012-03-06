26a2f5f4f66ea1e3d738d3da23f475296ce00af1
takatoh <takatoh.m@gmail.com>
2012-03-06 15:37:40 +0900

    bin/dip.rb: --remove option.

--- bin/dip.rb
+++ bin/dip.rb
@@ -10,7 +10,7 @@ require 'pathname'
 require 'optparse'
 
 
-SCRIPT_VERSION = "v0.1.0"
+SCRIPT_VERSION = "v0.1.1"
 
 options = {}
 opts = OptionParser.new
@@ -18,6 +18,7 @@ opts.banner =<<EOB
 #{opts.program_name} - Dip files from sub directories.
 Usage: #{opts.program_name} [option] DIR [DEST]
 EOB
+opts.on_tail('-r', '--remove', 'remove files in directories'){|v| options[:remove] = true}
 opts.on_tail('-h', '--help', 'show this message'){|v| puts opts; exit}
 opts.on_tail('-v', '--version', 'show version'){|v| puts SCRIPT_VERSION; exit}
 opts.parse!
@@ -37,7 +38,11 @@ Find.find(src) do |f|
   if file.file?
     puts f
     counter += 1
-    FileUtils.cp(f, dest + file.basename)
+    if options[:remove]
+      FileUtils.mv(f, dest + file.basename)
+    else
+      FileUtils.cp(f, dest + file.basename)
+    end
   end
 end
 
