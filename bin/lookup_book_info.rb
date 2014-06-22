712ed91992b3419157879c8a6dc9eb4925b0a4f3
takatoh <takatoh.m@gmail.com>
2014-06-22 17:33:45 +0900

    bin/lookup_book_info.rb: Update. v0.2.0.

--- bin/lookup_book_info.rb
+++ bin/lookup_book_info.rb
@@ -4,42 +4,50 @@
 # Look up information of book by ISBN with Amazon.
 #
 #
-# Rubygem `ruby-aaws' is required.
-# You can install the gem:
+# Rubygem `amazon-ecs' and `lisbn' are required.
+# You can install the gems:
 #
-# <tt>gem install ruby-aaws</tt>
+# <tt>gem install amazon-ecs</tt>
+# <tt>gem install lisbn</tt>
 #
 
-require 'amazon/aws/search'
+require 'amazon/ecs'
+require 'lisbn'
+require 'yaml'
 require 'optparse'
 
 
+SCRIPT_VERSION = "v0.2.0"
+
+@config = YAML.load_file(File.join(ENV["HOME"], "aws-config.yaml"))
+Amazon::Ecs.options = {
+  :associate_tag     => @config["ASSOCIATE_TAG"],
+  :AWS_access_key_id => @config["ACCESS_KEY_ID"],
+  :AWS_secret_key    => @config["SECRET_ACCESS_KEY"]
+}
+
 def search(code)
-  a = Amazon::AWS::ItemSearch.new("Books", {"Keywords" => code})
-  a.response_group = Amazon::AWS::ResponseGroup.new(:Medium)
-  req = Amazon::AWS::Search::Request.new
-  begin
-    res = req.search(a)
-    get_property(res.item_search_response.items.item)
-  rescue
-    Hash.new
-  end
+  asin = Lisbn.new(code).isbn10
+  res = Amazon::Ecs.item_lookup(
+    asin,
+    :response_group => "ItemAttributes",
+    :country => "jp")
+  element = res.items.first.get_element("ItemAttributes")
+  { 'title'        => element.get('Title'),
+    'author'       => element.get('Author'),
+    'publisher'    => element.get('Publisher'),
+    'manufacturer' => element.get('Manufacturer'),
+    'isbn'         => element.get('ISBN') }
 end
-
-def get_property(item)
-  asin = item.asin.to_s
-  attr = item.item_attributes.to_h
-  attr.each_key do |key|
-    attr[key] = attr[key].to_s
-  end
-  attr["asin"] ||= asin
-  attr
-end
-
 
 options = {}
 opt = OptionParser.new
+opt.banner = <<EOB
+Usage: #{opt.program_name} [options] ISBN
+EOB
 opt.on('-i', '--input=FILE', 'read ISBN from FILE.'){|f| options[:inputfile] = f}
+opt.on_tail('-h', '--help', 'show this message'){|v| print opt; exit}
+opt.on_tail('-v', '--version', 'show version'){|v| puts SCRIPT_VERSION; exit}
 opt.parse!
 
 codes = if options[:inputfile]
