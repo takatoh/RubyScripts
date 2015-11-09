33fa741e499a17908ff286d13464ee680d590d0e
takatoh <takatoh.m@gmail.com>
2015-11-09 21:24:11 +0900

    bin/dip.rb: Use Pathname class effectively.

--- bin/dip.rb
+++ bin/dip.rb
@@ -1,16 +1,15 @@
-#! ruby
+#!/usr/bin/env ruby
 # encoding: utf-8
 #
 #  Dip files from sub-directories.
 #
 
-require 'find'
-require 'fileutils'
 require 'pathname'
+require 'fileutils'
 require 'optparse'
 
 
-SCRIPT_VERSION = "v0.1.1"
+SCRIPT_VERSION = "v0.1.2"
 
 options = {}
 opts = OptionParser.new
@@ -18,34 +17,27 @@ opts.banner =<<EOB
 #{opts.program_name} - Dip files from sub directories.
 Usage: #{opts.program_name} [option] DIR [DEST]
 EOB
-opts.on_tail('-r', '--remove', 'remove files in directories'){|v| options[:remove] = true}
+opts.on_tail('-r', '--remove', 'remove files in sub directories'){|v| options[:remove] = true}
 opts.on_tail('-h', '--help', 'show this message'){|v| puts opts; exit}
 opts.on_tail('-v', '--version', 'show version'){|v| puts SCRIPT_VERSION; exit}
 opts.parse!
 
 
 src = Pathname.new(ARGV.shift)
-dest = ARGV.shift
-if dest
-  dest = Pathname.new(dest)
-else
-  dest = Pathname.new(".")
-end
+dest = Pathname.new(ARGV.shift || ".")
 
 counter = 0
-Find.find(src) do |f|
-  file = Pathname.new(f)
-  if file.file?
+src.find do |f|
+  if f.file?
     puts f
     counter += 1
     if options[:remove]
-      FileUtils.mv(f, dest + file.basename)
+      FileUtils.mv(f, dest + f.basename)
     else
-      FileUtils.cp(f, dest + file.basename)
+      FileUtils.cp(f, dest + f.basename)
     end
   end
 end
 
 puts ""
 puts "#{counter.to_s} files diped."
-
