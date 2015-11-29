cb2cb146b899df222f9541298edad321331a4982
takatoh <takatoh.m@gmail.com>
2015-11-29 14:15:19 +0900

    bin/post_book.rb: Update.

--- bin/post_book.rb
+++ bin/post_book.rb
@@ -3,31 +3,51 @@
 
 
 require 'httpclient'
+require 'csv'
 require 'json'
 require 'pp'
 
 
-post_url = "http://localhost:5000/api/book/add/"
-post_data = {
-  "title"          => "夏のロケット",
-  "volume"         => "",
-  "series"         => "",
-  "series_volume"  => "",
-  "author"         => "川端裕人",
-  "translator"     => "",
-  "publisher"      => "",
-  "category"       => "小説",
-  "format"         => "文庫",
-  "isbn"           => "",
-  "published_on"   => "",
-  "original_title" => "",
-  "note"           => "",
-  "keyword"        => "",
-  "disk"           => "",
-  "disposed"       => "0"
-}
+def post_book(book)
+  post_url = "http://localhost:9090/api/book/add/"
+  post_data = {
+    "title"          => book["title"],
+    "volume"         => book["volume"]         || "",
+    "series"         => book["series"]         || "",
+    "series_volume"  => book["series_volume"]  || "",
+    "author"         => book["author"]         || "",
+    "translator"     => book["translator"]     || "",
+    "publisher"      => book["publisher"]      || "",
+    "category"       => book["category"]       || "その他",
+    "format"         => book["format"]         || "その他",
+    "isbn"           => book["isbn"]           || "",
+    "published_on"   => book["published_on"]   || "",
+    "original_title" => book["original_title"] || "",
+    "note"           => book["note"]           || "",
+    "keyword"        => book["keyword"]        || "",
+    "disk"           => book["disk"]           || "",
+    "disposed"       => book["disposed"]       || "0"
+  }
 
-client = HTTPClient.new
-res = client.post(post_url, post_data)
-result = JSON.parse(res.body)
-pp result
+  res = @client.post(post_url, post_data)
+  result = JSON.parse(res.body)
+  puts title_with_vol(result["books"].first)
+end
+
+def title_with_vol(book)
+  if book["volume"].nil? || book["volume"].empty?
+    book["title"]
+  else
+    book["title"] + " [" + book["volume"] + "]"
+  end
+end
+
+
+@client = HTTPClient.new
+
+csvfile = File.open(ARGV.shift, "r")
+csv = CSV.new(csvfile, headers: true)
+csv.each do |row|
+#  puts title_with_vol(row)
+  post_book(row)
+end
