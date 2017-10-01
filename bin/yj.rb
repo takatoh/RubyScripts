41d75336eb7c276719e38f17b05346ce2f3ee7b6
takatoh <takatoh.m@gmail.com>
2017-10-01 17:24:47 +0900

    bin/yj.rb: New.

---
+++ bin/yj.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'yaml'
+require 'json'
+
+def yaml2json(src)
+	doc = YAML.load_file(src)
+	JSON.pretty_generate(doc)
+end
+
+def json2yaml(src)
+	doc = nil
+	File.open(src) do |f|
+		doc = JSON.parse(f.read)
+	end
+	doc.to_yaml
+end
+
+src = ARGV.shift
+ext = File.extname(src).downcase
+if ext == ".yaml" || ext == ".yml"
+	puts yaml2json(src)
+elsif ext == ".json"
+	puts json2yaml(src)
+else
+	puts "Unsupported format."
+end
