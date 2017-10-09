a6b55af8ed54af47dd076d4a7f54176050deaedb
takatoh <takatoh.m@gmail.com>
2017-10-09 18:22:06 +0900

    bin/yj.rb: Translate tabs to spaces.

--- bin/yj.rb
+++ bin/yj.rb
@@ -5,24 +5,24 @@ require 'yaml'
 require 'json'
 
 def yaml2json(src)
-	doc = YAML.load_file(src)
-	JSON.pretty_generate(doc)
+  doc = YAML.load_file(src)
+  JSON.pretty_generate(doc)
 end
 
 def json2yaml(src)
-	doc = nil
-	File.open(src) do |f|
-		doc = JSON.parse(f.read)
-	end
-	doc.to_yaml
+  doc = nil
+  File.open(src) do |f|
+    doc = JSON.parse(f.read)
+  end
+  doc.to_yaml
 end
 
 src = ARGV.shift
 ext = File.extname(src).downcase
 if ext == ".yaml" || ext == ".yml"
-	puts yaml2json(src)
+  puts yaml2json(src)
 elsif ext == ".json"
-	puts json2yaml(src)
+  puts json2yaml(src)
 else
-	puts "Unsupported format."
+  puts "Unsupported format."
 end
