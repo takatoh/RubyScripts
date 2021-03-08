6556a7d4af375fa567fe3a966e8ef4fbc885c88e
takatoh <takatoh.m@gmail.com>
2021-03-09 07:00:12 +0900

    bin/sweep.rb: Unify string literal quotes into double quotes.

--- bin/sweep.rb
+++ bin/sweep.rb
@@ -4,10 +4,10 @@
 # sweep.rb - Sweep files in the directory.
 #
 
-require 'pathname'
-require 'fileutils'
-require 'date'
-require 'optparse'
+require "pathname"
+require "fileutils"
+require "date"
+require "optparse"
 
 
 SCRIPT_VERSION = "v0.2.5"
@@ -21,23 +21,23 @@ def main
   #{opts.program_name} - Sweep files in the directory.
   Usage: #{opts.program_name} [options] <dir>
 EOB
-  opts.on('-d', '--dry-run', 'dry running'){|v| options[:dry_run] = true}
-  opts.on('-t', '--types=TYPES', 'remove specified types(comma separated)') do |v|
+  opts.on("-d", "--dry-run", "dry running"){|v| options[:dry_run] = true}
+  opts.on("-t", "--types=TYPES", "remove specified types(comma separated)") do |v|
     options[:types] = v.split(",").map{|t| t.downcase!; t[0,1] == "." ? t : "." + t}
   end
-  opts.on('-a', '--all-types', 'remove all types'){|v| options[:all_types] = true}
-  opts.on('-e', '--except-types=TYPES', 'except specified types(comma separated)') do |v|
+  opts.on("-a", "--all-types", "remove all types"){|v| options[:all_types] = true}
+  opts.on("-e", "--except-types=TYPES", "except specified types(comma separated)") do |v|
     options[:except_types] = v.split(",").map{|t| t.downcase!; t[0,1] == "." ? t : "." + t}
   end
-  opts.on('--not-image', "except following types: #{FileSweeper::IMAGE_TYPES.join(",")}") do |v|
+  opts.on("--not-image", "except following types: #{FileSweeper::IMAGE_TYPES.join(",")}") do |v|
     options[:not_image] = true
   end
-  opts.on('-T', '--time-distance=N', "older than N days ago") do |v|
+  opts.on("-T", "--time-distance=N", "older than N days ago") do |v|
     options[:distance] = v.to_i
   end
-  opts.on('--empty-dir', 'remove empty directories'){|v| options[:empty_dir] = true}
-  opts.on_tail('-h', '--help', 'show this message'){|v| puts opts; exit}
-  opts.on_tail('-v', '--version', 'show version'){|v| puts SCRIPT_VERSION; exit}
+  opts.on("--empty-dir", "remove empty directories"){|v| options[:empty_dir] = true}
+  opts.on_tail("-h", "--help", "show this message"){|v| puts opts; exit}
+  opts.on_tail("-v", "--version", "show version"){|v| puts SCRIPT_VERSION; exit}
   opts.parse!
 
   dir = Pathname.new(ARGV.shift)
