7290318dc1db718d3ea475d983043589092432b3
takatoh <takatoh.m@gmail.com>
2021-03-09 06:26:54 +0900

    lib/human_bytes.rb: Fix bug.

--- lib/human_bytes.rb
+++ lib/human_bytes.rb
@@ -7,12 +7,13 @@ module HumanBytes
   def human_bytes(bytes)
     units = %w( B KB MB GB TB PB EB ZB YB )
     u = 0
-    until bytes < 1000
+    until bytes < 1000.0
       bytes = bytes / 1000.0
       u += 1
     end
-    d = bytes > 100 ? 0 : (bytes > 10 ? 1 : 2)
-    roundoff(bytes, d).to_s + units[u]
+    d = bytes >= 100 ? 0 : (bytes >= 10 ? 1 : 2)
+#    roundoff(bytes, d).to_s + units[u]
+    sprintf("%.#{d}f#{units[u]}", bytes)
   end
   module_function :human_bytes
 
