1857682370d1eeb5fc0e60acb7e9a09c5fc53d58
takatoh <takatoh.m@gmail.com>
2021-03-09 07:04:27 +0900

    bin/sweep.rb: Tweak.

--- bin/sweep.rb
+++ bin/sweep.rb
@@ -21,13 +21,13 @@ def main
   #{opts.program_name} - Sweep files in the directory.
   Usage: #{opts.program_name} [options] <dir>
 EOB
-  opts.on("-d", "--dry-run", "dry running"){|v| options[:dry_run] = true}
+  opts.on("-d", "--dry-run", "dry running"){|v| options[:dry_run] = true }
   opts.on("-t", "--types=TYPES", "remove specified types(comma separated)") do |v|
-    options[:types] = v.split(",").map{|t| t.downcase!; t[0,1] == "." ? t : "." + t}
+    options[:types] = v.split(",").map{|t| t.downcase!; t[0,1] == "." ? t : "." + t }
   end
-  opts.on("-a", "--all-types", "remove all types"){|v| options[:all_types] = true}
+  opts.on("-a", "--all-types", "remove all types"){|v| options[:all_types] = true }
   opts.on("-e", "--except-types=TYPES", "except specified types(comma separated)") do |v|
-    options[:except_types] = v.split(",").map{|t| t.downcase!; t[0,1] == "." ? t : "." + t}
+    options[:except_types] = v.split(",").map{|t| t.downcase!; t[0,1] == "." ? t : "." + t }
   end
   opts.on("--not-image", "except following types: #{FileSweeper::IMAGE_TYPES.join(",")}") do |v|
     options[:not_image] = true
@@ -35,9 +35,9 @@ EOB
   opts.on("-T", "--time-distance=N", "older than N days ago") do |v|
     options[:distance] = v.to_i
   end
-  opts.on("--empty-dir", "remove empty directories"){|v| options[:empty_dir] = true}
-  opts.on_tail("-h", "--help", "show this message"){|v| puts opts; exit}
-  opts.on_tail("-v", "--version", "show version"){|v| puts SCRIPT_VERSION; exit}
+  opts.on("--empty-dir", "remove empty directories"){|v| options[:empty_dir] = true }
+  opts.on_tail("-h", "--help", "show this message"){|v| puts opts; exit }
+  opts.on_tail("-v", "--version", "show version"){|v| puts SCRIPT_VERSION; exit }
   opts.parse!
 
   dir = Pathname.new(ARGV.shift)
@@ -48,7 +48,7 @@ end
 
 class FileSweeper
 
-  IMAGE_TYPES = %w(.jpg .jpeg .png .bmp .gif .tif .tiff .tga .pcx)
+  IMAGE_TYPES = %w( .jpg .jpeg .png .bmp .gif .tif .tiff .tga .pcx )
 
   def initialize(dir, opts)
     @dir = Pathname.new(dir)
@@ -67,16 +67,16 @@ class FileSweeper
   def set_conds
     @conds = []
     if @options[:all_types]
-      @conds << lambda{|f| true}
+      @conds << lambda{|f| true }
     end
     if @options[:types]
-      @conds << lambda{|f| @options[:types].include?(f.extname.downcase)}
+      @conds << lambda{|f| @options[:types].include?(f.extname.downcase) }
     end
     if @options[:except_types]
-      @conds << lambda{|f| !(@options[:except_types].include?(f.extname.downcase))}
+      @conds << lambda{|f| !(@options[:except_types].include?(f.extname.downcase)) }
     end
     if @options[:not_image]
-      @conds << lambda{|f| not_image?(f)}
+      @conds << lambda{|f| not_image?(f) }
     end
     if @options[:distance]
       @conds << lambda do |f|
@@ -87,7 +87,7 @@ class FileSweeper
   end
 
   def sweep_files
-    @dir.children.select{|c| c.file?}.each do |c|
+    @dir.children.select{|c| c.file? }.each do |c|
       if match?(c)
         if @options[:dry_run]
           puts c
@@ -100,7 +100,7 @@ class FileSweeper
   end
 
   def sweep_directories
-    @dir.children.select{|c| c.directory?}.each do |c|
+    @dir.children.select{|c| c.directory? }.each do |c|
       FileSweeper.new(c, @options).sweep
       if @options[:empty_dir] && c.children.empty?
         if @options[:dry_run]
@@ -114,7 +114,7 @@ class FileSweeper
   end
 
   def match?(p)
-    @conds.empty? ? false : @conds.all?{|c| c.call(p)}
+    @conds.empty? ? false : @conds.all?{|c| c.call(p) }
   end
 
   def not_image?(p)
